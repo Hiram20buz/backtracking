c12217a8a9462438f7212561194e3beed85b88bb
Hiram20buz <l20211847@tectijuana.edu.mx>
2024-02-04 15:54:18 -0800

    back

--- 1_example.py
+++ 1_example.py
@@ -3,3 +3,11 @@ Ejemplo: Cree un programa que muestre todos los string de
 largo 3 que se pueden formar a partir de las letras "a", "b" y
 "c".
 '''
+def permutaciones(s) :
+    if(len(s) == 3):
+        print(s)
+        return
+    for c in ['a', 'b', 'c']:
+        permutaciones(s + c)
+
+permutaciones ("")
---
+++ 1_notes.md
@@ -0,0 +1,6 @@
+Idea: En forma ordenada y exhaustiva exploraremos todas las combinaciones posibles para solucionar un problema.
+
+
+Ejemplo: Laberinto.
+
+## ¿Cuales son los elementos principales de una solucion mediante backtracking?
